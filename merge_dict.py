2fdc5080f11550258f3e2fceeaa1a06101c58745
Shahana14401 <119149935+Shahana14401@users.noreply.github.com>
2022-12-08 14:21:18 +0530

    Create merge_dict.py
    Merge two dictionaries

---
+++ merge_dict.py
@@ -0,0 +1,9 @@
+#merge two dictionaries
+def merge(dict1,dict2):
+    return(dict1.update(dict2))
+dict1={'a':10,'b':8}
+dict2={'d':6,'c':4}
+print("First dictionary is:",dict1)
+print("Second dictionary is:",dict2)
+merge(dict1,dict2)
+print("Merged dictionary is:",dict1)
